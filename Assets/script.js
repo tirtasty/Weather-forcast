02b45847c52de07eb9a0d457c35009e9da862c5f
Tirta Styadi <tirta.setiady@yahoo.com>
2022-01-13 16:47:40 +1100

    Add Javascript

---
+++ Assets/script.js
@@ -0,0 +1,4 @@
+var cityFormEl = document.querySelector('#user-form');
+var cityInputEl = document.querySelector('#cityname');
+var cityContainerEl = document.querySelector('#city-container');
+var citySearchTerm = document.querySelector('#city-search-term');
