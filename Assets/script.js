84cdc2ff8b08b5137d004c8ab2fb3bc4c4839f01
Tirta Styadi <tirta.setiady@yahoo.com>
2022-01-13 18:36:34 +1100

    ADD: Add function for display temperature, wind, and humidity

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,12 @@ var cityFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#cityname');
 var cityContainerEl = document.querySelector('#city-container');
 var citySearchTerm = document.querySelector('#city-search-term');
+var temp = document.querySelector('#temp');
+var humidity = document.querySelector('#humidity');
+var wind = document.querySelector('#wind');
+var humidity = document.querySelector('#humidity');
+var uvIndex = document.querySelector('#uvIndex');
+var currentDay = moment().format('dddd/MM/YY')
 
 // Submit Button For Weather
 var formSubmitHandler = function (event) {
@@ -10,10 +16,10 @@ var formSubmitHandler = function (event) {
     var cityName = cityInputEl.value.trim();
   
     if (cityName) {
-      //need to change based on city name
+      // Change based on city name
       getWeather(cityName);
-  
-      citySearchTerm.textContent = cityName;
+      // Display city name on dashboard and empty the input column.
+      citySearchTerm.textContent = cityName + ' - ' +currentDay;
       cityInputEl.value = '';
     } else {
       alert('Please enter a City name!');
@@ -24,17 +30,18 @@ var formSubmitHandler = function (event) {
   
   // Function to get city's weather
   var getWeather = function (city) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=690b389bbce06cc3766da916b5049efd';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=690b389bbce06cc3766da916b5049efd';
     fetch(apiUrl)
       .then(function (response) {
         
         if (response.ok) {
           response.json().then(function (data) {
+              var windSpeed = Math.round(data.wind.speed);
+              temp.textContent = Math.round(data.main.temp) - 273 + "\u00B0 Celcius";
+              wind.textContent = windSpeed.toFixed(1) * 3.6 + " KPH";
+              humidity.textContent = Math.round(data.main.humidity) + "%";
           console.log(data)
-           // console.log(data.weather[0].main);
-           // console.log(data.main.temp) 
-           // repoContainerEl.textContent = data.main.temp-273 + " Celcius";
-            // displayRepos(data, city);
+             console.log(data.weather[0].main);
           });
         } else {
           alert('Error: ' + response.statusText);
@@ -48,4 +55,6 @@ var formSubmitHandler = function (event) {
   
   
   cityFormEl.addEventListener('submit', formSubmitHandler);
-  
+
+
+  // forecast weather for 5 days 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=690b389bbce06cc3766da916b5049efd'
