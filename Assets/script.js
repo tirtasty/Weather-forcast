69c8f60655fb6ffbc26988267a6105d939c414b6
Tirta Styadi <tirta.setiady@yahoo.com>
2022-01-15 21:12:31 +1100

    Adding style and sync with javascrip data

--- Assets/script.js
+++ Assets/script.js
@@ -5,28 +5,41 @@ var citySearchTerm = document.querySelector('#city-search-term');
 var temp = document.querySelector('#temp');
 var humidity = document.querySelector('#humidity');
 var wind = document.querySelector('#wind');
+var historySearch = document.querySelector("history-search");
 var humidity = document.querySelector('#humidity');
+var forecastCard = document.querySelector("forecast")
 var uvIndex = document.querySelector('#uvIndex');
 var currentDay = moment().format('dddd/MM/YY')
+
+// Forecast for 5 Days
+var firstDay = document.querySelector('#firstDay');
+var secondDay = document.querySelector('#secondDay');
+var thirdDay = document.querySelector('#thirdDay');
+var forthDay = document.querySelector('#forthDay');
+var fifthDay = document.querySelector('#fifthDay');
+var forecastFiveDays = document.getElementsByClassName("column")
+
+// Setup Local Storage
+
 
 // Submit Button For Weather
 var formSubmitHandler = function (event) {
     event.preventDefault();
   
     var cityName = cityInputEl.value.trim();
-  
     if (cityName) {
       // Change based on city name
       getWeather(cityName);
+      ;
       // Display city name on dashboard and empty the input column.
       citySearchTerm.textContent = cityName + ' - ' +currentDay;
       cityInputEl.value = '';
     } else {
       alert('Please enter a City name!');
     }
-    console.log(cityName);
   };
   
+    
   
   // Function to get city's weather
   var getWeather = function (city) {
@@ -49,13 +62,14 @@ var formSubmitHandler = function (event) {
               var lat = data.coord.lat;
               console.log(lon, lat)
               //Getting More Data from One Call API for UV Index
-              var testUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=690b389bbce06cc3766da916b5049efd";
-              fetch (testUrl)
+              var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=690b389bbce06cc3766da916b5049efd";
+              fetch (forecastUrl)
               .then (function(response) {
               if (response.ok){
               response.json().then(function(dataOneCall){
                 console.log(dataOneCall)
                 uvIndex.textContent = dataOneCall.current.uvi;
+                // When value of UV Index less than 2, background will be green and if the value more than 2 background will be red
                 if (dataOneCall.current.uvi < 2){
                   uvIndex.style.backgroundColor = "lightgreen"
                   uvIndex.style.color = "blue"
@@ -63,12 +77,14 @@ var formSubmitHandler = function (event) {
                   uvIndex.style.backgroundColor = "red"
                   uvIndex.style.color = "white"
                 }
+
+              // 5 Days Forecast
+              firstDay
+
+             
         })
       }
-    })
-              
-
-              
+    });        
           });
         } else {
           alert('Error: ' + response.statusText);
@@ -80,6 +96,10 @@ var formSubmitHandler = function (event) {
   };
   
   
+
+
+
+
   
   cityFormEl.addEventListener('submit', formSubmitHandler);
 
